bbe3fa93fd36afcdaf0b80f5a51592f8febcc443
manu <crayonwoods@gmail.com>
2022-08-30 16:36:20 -0300

    configs finais

--- core/models.py
+++ core/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
+
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.descricao
 
 
 class Editora(models.Model):
@@ -16,14 +14,16 @@ class Editora(models.Model):
 
 
 class Autor(models.Model):
-    nome = models.CharField(max_length=255)
-    email = models.EmailField()
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=50)
 
     def __str__(self):
         return self.nome
 
-    class Meta:
-        verbose_name_plural = "Autores"
+
+class Meta:
+    verbose_name_plural = "Autores"
+
 
 class Livro(models.Model):
     titulo = models.CharField(max_length=255)
--- core/serializers.py
+++ core/serializers.py
@@ -1,13 +1,20 @@
 from rest_framework.serializers import ModelSerializer
 
-from core.models import Categoria, Editora
+from core.models import Autor
+from core.models import Categoria
+from core.models import Editora
+from core.models import Livro
+
+class AutorSerializer(ModelSerializer):
+    class Meta:
+        model = Autor
+        fields = "__all__"
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
         model = Categoria
         fields = "__all__"
-
-
+        
 class EditoraSerializer(ModelSerializer):
     class Meta:
         model = Editora
