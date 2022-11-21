66af68d8d830217e423cfae0ea65ced87b610406
Gabriel-Quadros <gquadros312@gmail.com>
2022-11-21 11:33:06 -0300

    aula 25

--- livraria/settings.py
+++ livraria/settings.py
@@ -9,29 +9,26 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import environ
+
 import os
 from pathlib import Path
+import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-
 env = environ.Env()
 environ.Env.read_env((os.path.join(BASE_DIR, '.env')))
 
 SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
+
+DATABASES = {'default': env.db()}
 
 # Application definition
 
